eb38da04f86a9524e0076adab754fe72e29f736f
enriquemdev <enriquemunozdev@gmail.com>
2025-03-17 14:22:11 -0600

    organized initializing services

--- index.ts
+++ index.ts
@@ -1,12 +1,46 @@
 import express, { Express, Request, Response } from "express";
+const { Client } = require('whatsapp-web.js');
 
 const app: Express = express();
+const client = new Client();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('test');
-});
+async function initializeWhatsappClient () {
+    try {
+        client.once('ready', () => {
+            console.log('Whatsapp client is ready!');
+        });
+        
+        client.on('qr', (qr: any) => {
+            console.log('Whatsapp QR RECEIVED', qr);
+        });
 
-app.listen(port, () => {
-    console.log(`Servidor iniciado en http://localhost:${port}`);
-})
+        await client.initialize();
+        console.log('Whatsapp client initialized');
+    } catch (error) {
+        console.error('Error initializing whatsapp client:', error);
+    }
+}
+
+async function startWebServer() {
+    try {
+        await initializeWhatsappClient();
+
+        // WEB ROUTES
+
+        app.get('/', (req: Request, res: Response) => {
+            res.send('test');
+        });
+
+
+        // END WEB ROUTES
+
+        app.listen(port, () => {
+            console.log(`Servidor iniciado en http://localhost:${port}`);
+        })
+    } catch (error) {
+        console.error('Error starting web server:', error);
+    }
+}
+
+startWebServer();
