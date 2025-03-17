c3308500cf0abbaf34c9d498b247afb675b303f0
enriquemdev <enriquemunozdev@gmail.com>
2025-03-17 11:31:59 -0600

    finished starting point of the express app with ts and updated gitignore

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('test');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor iniciado en http://localhost:${port}`);
+})
